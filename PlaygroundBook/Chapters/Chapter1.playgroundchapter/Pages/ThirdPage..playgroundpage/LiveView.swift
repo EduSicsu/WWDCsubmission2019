a261fd746679ec3fd898a94dab17a3a0e36b0cb0
Eduardo Oliveira Lopes <eduardooliveiralopes97@gmail.com>
2019-03-19 18:38:02 -0400

    Third page created (test)

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/ThirdPage..playgroundpage/LiveView.swift
@@ -0,0 +1,14 @@
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  Instantiates a live view and passes it to the PlaygroundSupport framework.
+//
+
+import UIKit
+import PlaygroundSupport
+
+private let liveViewIdentifier: String = "ThirdPage"
+
+// Instantiate a new instance of the live view from the book's auxiliary sources and pass it to PlaygroundSupport.
+PlaygroundPage.current.liveView = instantiateLiveView(identifier: liveViewIdentifier)
