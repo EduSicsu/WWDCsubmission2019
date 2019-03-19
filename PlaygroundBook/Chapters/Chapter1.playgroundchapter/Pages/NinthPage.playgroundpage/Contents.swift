132df8e42940475234574d736faceb2a4654db11
Eduardo Oliveira Lopes <eduardooliveiralopes97@gmail.com>
2019-03-19 19:45:23 -0400

    new chapters on playgrounds

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/NinthPage.playgroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+ 
+ #  Apps for everyone
+  
+ Welcome, today we gonna talk a little bit about accessibility, and how important it is for everybody, especially for us. In this world we got people who do not have the same opportunities like us, they has limitations.
+ 
+ But these is not a problem, because they're still sensational, because they can do amazing things and provide life experiences you won't believe.
+ 
+ This playgrounds is for them
+*/
+
+let str = "Hello, playground"
