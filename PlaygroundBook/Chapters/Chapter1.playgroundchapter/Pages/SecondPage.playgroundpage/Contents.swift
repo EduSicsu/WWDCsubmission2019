61af252c6b87090acd5019e366485505c629fc2c
Eduardo Oliveira Lopes <eduardooliveiralopes97@gmail.com>
2019-03-19 19:38:43 -0400

    Spaces for the pages.

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/SecondPage.playgroundpage/Contents.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/SecondPage.playgroundpage/Contents.swift
@@ -1,9 +1,11 @@
-//#-hidden-code
-//
-//  See LICENSE folder for this template’s licensing information.
-//
-//  Abstract:
-//  The Swift file containing the source code edited by the user of this playground book.
-//
-//#-end-hidden-code
+/*:
+ 
+ # About me
+ 
+ First of wall, my name is Eduardo Oliveira Lopes, I have 22 years old and I'm design student. So, nice to meet you.
+ 
+ In less than one year everything has completely changed in my life, and I have no idea how important is accessibility, after that I researched a lot about this, and I also discovered the developers do not give the value they should for that topic.
+ 
+ I felt bad for this, so many people are ignored I needed to do something about that, so when the submissions for the scholarship were opened. I did not think twice about what the theme of my project would be.
+ */
 let str = "Hello, playground"
---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/ThirdPage.playgroundpage/Contents.swift
@@ -0,0 +1,17 @@
+/*:
+ 
+ # Accessibility and tools
+ 
+ When we talk about accessibility Apple has their own tools
+ 
+ - Captioning and Audio Descriptions
+ - VoiceOver
+ - Display Customization
+ - Guided Access
+ 
+ And lot more, and for an application to be accessible to certain audiences now we go help some people applying basic concepts of this tools above.
+ 
+ And in this playgrounds we will learn some basics concepts of how to help people using: Display Customization and VoiceOver.
+ 
+ */
+let str = "Hello, playground"
